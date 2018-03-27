363615fdb10e346dafadc40cb54d17e4a2a893aa
favarete <rodrigo.favarete@hotmail.com>
2018-03-27 15:23:05 -0300

    Add database of valid visual matrices

---
+++ database.py
@@ -0,0 +1,13 @@
+#The pattern used must begin with the front pointed up and 
+#turning clockwise: Front points Top -> Right -> Down -> Left
+
+PATTERN = { "ID::0000": [[0, 1, 0, 1, 0, 1, 1, 0, 0],
+				  		[1, 1, 0, 0, 0, 1, 0, 1, 0],
+				  		[0, 0, 1, 1, 0, 1, 0, 1, 0],
+				  		[0, 1, 0, 1, 0, 0, 0, 1, 1]],
+
+			"ID::0001": [[1, 0, 0, 0, 1, 0, 1, 0, 1],
+				  		[1, 0, 1, 0, 1, 0, 1, 0, 0],
+				  		[1, 0, 1, 0, 1, 0, 0, 0, 1],
+				  		[0, 0, 1, 0, 1, 0, 1, 0, 1]]
+}
