2e7d776067f7a34905c8e0e6fb7c43049235d20f
favarete <rodrigo.favarete@hotmail.com>
2018-05-30 03:32:55 -0300

    Saves snaps from the webcam for calibration function

---
+++ camera/collect_data.py
@@ -0,0 +1,29 @@
+from webcam import Webcam
+import cv2
+import uuid
+from pathlib import Path
+  
+camera = 1
+
+chess_pattern_path = Path.cwd() / "sample_images" / "pattern" / "chessboard"
+if chess_pattern_path.exists() == False:
+    chess_pattern_path.mkdir(parents=True)
+
+webcam = Webcam(camera)
+webcam.start()
+
+try:
+    while True:
+        img = webcam.get_current_frame()
+        cv2.imshow('capture', img)
+        cv2.waitKey(500)
+        
+        ret, corners = cv2.findChessboardCorners(cv2.cvtColor(img,cv2.COLOR_BGR2GRAY), (7,6), None)
+        
+        if ret == True:
+            filename = "sample" + str(uuid.uuid4().hex) + '.jpg'
+            cv2.imwrite(str(chess_pattern_path / filename), img)
+
+except KeyboardInterrupt:
+    webcam.destroy()
+    cv2.destroyAllWindows()
