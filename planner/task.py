7402bd5c5a2705dfd83ad88d4c729175eaec7742
favarete <rodrigo.favarete@hotmail.com>
2018-06-03 06:01:55 -0300

    Add global scope and UI access into task

--- planner/task.py
+++ planner/task.py
@@ -1,6 +1,7 @@
 from database import PATTERN, TASK
 
 import cv2
+import _global_
 from utils import *
 
 DISTORTION = .4
@@ -18,7 +19,7 @@ TASK_COLOR = (70, 80, 255)
 SOLUTION_COLOR = (255, 255, 0)
 LINE_THICKNESS = 4 
 
-def process_frame(contours, img_rgb, img_gray, task_manager, robots_manager):
+def process_frame(contours, img_gray, visible_img):
 
 	for c in contours:
 		# Approximate the contour
@@ -38,8 +39,8 @@ def process_frame(contours, img_rgb, img_gray, task_manager, robots_manager):
 	 				
 	 		glyph_pattern = get_glyph_pattern(resized_shape)
 
-	 		#Draw all squares found
-			#cv2.drawContours(img_rgb, [approx], -1, DIMENSIONS_COLOR, LINE_THICKNESS/2)
+	 		if _global_.gui_properties["section_1b"]["all_squares_view"]:
+				cv2.drawContours(visible_img, [approx], -1, DIMENSIONS_COLOR, LINE_THICKNESS/2)
 
 			for key, value in PATTERN.iteritems():
 				for i in range(4):
@@ -50,21 +51,22 @@ def process_frame(contours, img_rgb, img_gray, task_manager, robots_manager):
 						frontX, frontY = get_pattern_front(i, ordered_points)
 						cX, cY = get_central_points(c)
 						Xc, Yc = get_extended_point(cX, cY, frontX, frontY, robot_radius)
-						robots_manager[key]["node"][0] = [cX, cY]
-						robots_manager[key]["node"][1] = [Xc, Yc]
-						robots_manager[key]["indetified"] = True
-						robots_manager[key]["radius"] = robot_radius
-						cv2.drawContours(img_rgb, [approx], -1, DIMENSIONS_COLOR, LINE_THICKNESS)
-						cv2.line(img_rgb, (cX, cY), (Xc, Yc), DIMENSIONS_COLOR, LINE_THICKNESS)
-						cv2.circle(img_rgb, (cX, cY), DOT_SIZE, DIMENSIONS_COLOR, -1)
-						cv2.circle(img_rgb, (cX, cY), robot_radius, DIMENSIONS_COLOR, LINE_THICKNESS)
-						cv2.putText(img_rgb, key, 
-									(cX - 5, cY - 15), 
-									FONT_BIG, 
-									TEXT_SIZE_BIG, 
-									FONT_COLOR, 
-									BOLD, 
-									cv2.LINE_AA)
+						_global_.robots_manager[key]["node"][0] = [cX, cY]
+						_global_.robots_manager[key]["node"][1] = [Xc, Yc]
+						_global_.robots_manager[key]["indetified"] = True
+						_global_.robots_manager[key]["radius"] = robot_radius
+						if _global_.gui_properties["section_1b"]["detection_view"]:
+							cv2.drawContours(visible_img, [approx], -1, DIMENSIONS_COLOR, LINE_THICKNESS)
+							cv2.line(visible_img, (cX, cY), (Xc, Yc), DIMENSIONS_COLOR, LINE_THICKNESS)
+							cv2.circle(visible_img, (cX, cY), DOT_SIZE, DIMENSIONS_COLOR, -1)
+							cv2.circle(visible_img, (cX, cY), robot_radius, DIMENSIONS_COLOR, LINE_THICKNESS)
+							cv2.putText(visible_img, key, 
+										(cX - 5, cY - 15), 
+										FONT_BIG, 
+										TEXT_SIZE_BIG, 
+										FONT_COLOR, 
+										BOLD, 
+										cv2.LINE_AA)
 						break
 				if robot_found:
 					break
@@ -75,27 +77,28 @@ def process_frame(contours, img_rgb, img_gray, task_manager, robots_manager):
 						if glyph_pattern == value["matrix"][i]: 
 							task_found = True
 							#TODO: Use this if it's busy on some task
-							task_manager["solve_task"] = True
+							_global_.task_manager["solve_task"] = True
 							frontX, frontY = get_pattern_front(i, ordered_points)
 							centerX, centerY = get_central_points(c)
 							# List of solutions for the known tasks
 							if key == "ID::A":
 								topX, topY = get_extended_point(centerX, centerY, frontX, frontY, value["dimension"])
 								solution_points = get_polyline_list([topX, topY])
-								task_manager["solution_points"] = solution_points
-								task_manager["busy_robots"] += value["difficult"]
+								_global_.task_manager["solution_points"] = solution_points
+								_global_.task_manager["busy_robots"] += value["difficult"]
 								#TODO: Use this if it's busy on some task
-								task_manager["task_ID"] = key
-								cv2.circle(img_rgb, (topX, topY), DOT_SIZE, SOLUTION_COLOR, -1)
-										
-								cv2.polylines(img_rgb, [solution_points], True, SOLUTION_COLOR, 1)
-								cv2.putText(img_rgb, "Solution", 
-											(topX - 5, topY - 15), 
-											FONT_BIG, 
-											TEXT_SIZE_BIG, 
-											SOLUTION_COLOR, 
-											BOLD, 
-											cv2.LINE_AA)
+								_global_.task_manager["task_ID"] = key
+
+								if _global_.gui_properties["section_1b"]["detection_view"]:
+									cv2.circle(visible_img, (topX, topY), DOT_SIZE, SOLUTION_COLOR, -1)
+									cv2.polylines(visible_img, [solution_points], True, SOLUTION_COLOR, 1)
+									cv2.putText(visible_img, "Solution", 
+												(topX - 5, topY - 15), 
+												FONT_BIG, 
+												TEXT_SIZE_BIG, 
+												SOLUTION_COLOR, 
+												BOLD, 
+												cv2.LINE_AA)
 									
 							elif key == "ID::B":
 								topX, topY = get_extended_point(centerX, centerY, frontX, frontY, value["dimension"][0])
@@ -103,56 +106,60 @@ def process_frame(contours, img_rgb, img_gray, task_manager, robots_manager):
 								auxX, auxY = get_extended_point(baseX, baseY, centerX, centerY, -value["dimension"][2])
 								baseLeftX, baseLeftY, baseRightX, baseRightY = get_perpendicular_points(baseX, baseY, auxX, auxY)
 								solution_points = get_polyline_list([topX, topY, baseLeftX, baseLeftY, baseRightX, baseRightY])
-								task_manager["solution_points"] = solution_points
-								task_manager["busy_robots"] += value["difficult"]
+								_global_.task_manager["solution_points"] = solution_points
+								_global_.task_manager["busy_robots"] += value["difficult"]
 								#TODO: Use this if it's busy on some task
-								task_manager["task_ID"] = key
-								cv2.circle(img_rgb, (topX, topY), DOT_SIZE, SOLUTION_COLOR, -1)
-								cv2.circle(img_rgb, (baseLeftX, baseLeftY), DOT_SIZE, SOLUTION_COLOR, -1)
-								cv2.circle(img_rgb, (baseRightX, baseRightY), DOT_SIZE, SOLUTION_COLOR, -1)										
-								cv2.polylines(img_rgb, [solution_points], True, SOLUTION_COLOR, 1)
-								cv2.putText(img_rgb, "Solution", 
-											(topX - 5, topY - 15), 
-											FONT_BIG, 
-											TEXT_SIZE_BIG, 
-											SOLUTION_COLOR, 
-											BOLD, 
-											cv2.LINE_AA)
+								_global_.task_manager["task_ID"] = key
+
+								if _global_.gui_properties["section_1b"]["detection_view"]:
+									cv2.circle(visible_img, (topX, topY), DOT_SIZE, SOLUTION_COLOR, -1)
+									cv2.circle(visible_img, (baseLeftX, baseLeftY), DOT_SIZE, SOLUTION_COLOR, -1)
+									cv2.circle(visible_img, (baseRightX, baseRightY), DOT_SIZE, SOLUTION_COLOR, -1)										
+									cv2.polylines(visible_img, [solution_points], True, SOLUTION_COLOR, 1)
+									cv2.putText(visible_img, "Solution", 
+												(topX - 5, topY - 15), 
+												FONT_BIG, 
+												TEXT_SIZE_BIG, 
+												SOLUTION_COLOR, 
+												BOLD, 
+												cv2.LINE_AA)
 
 							elif key == "ID::C":
 								topX, topY = get_extended_point(centerX, centerY, frontX, frontY, value["dimension"])
 								baseX, baseY = get_extended_point(centerX, centerY, frontX, frontY, -value["dimension"])
 								solution_points = get_polyline_list([topX, topY, baseX, baseY])
-								task_manager["solution_points"] = solution_points
-								task_manager["busy_robots"] += value["difficult"]
+								_global_.task_manager["solution_points"] = solution_points
+								_global_.task_manager["busy_robots"] += value["difficult"]
 								#TODO: Use this if it's busy on some task
-								task_manager["task_ID"] = key
-								cv2.circle(img_rgb, (topX, topY), DOT_SIZE, SOLUTION_COLOR, -1)
-								cv2.circle(img_rgb, (baseX, baseY), DOT_SIZE, SOLUTION_COLOR, -1)
-								cv2.polylines(img_rgb, [solution_points], True, SOLUTION_COLOR, 1)
-								cv2.putText(img_rgb, "Solution", 
-											(topX - 5, topY - 15), 
+								_global_.task_manager["task_ID"] = key
+
+								if _global_.gui_properties["section_1b"]["detection_view"]:
+									cv2.circle(visible_img, (topX, topY), DOT_SIZE, SOLUTION_COLOR, -1)
+									cv2.circle(visible_img, (baseX, baseY), DOT_SIZE, SOLUTION_COLOR, -1)
+									cv2.polylines(visible_img, [solution_points], True, SOLUTION_COLOR, 1)
+									cv2.putText(visible_img, "Solution", 
+												(topX - 5, topY - 15), 
+												FONT_BIG, 
+												TEXT_SIZE_BIG, 
+												SOLUTION_COLOR, 
+												BOLD, 
+												cv2.LINE_AA)
+
+							if _global_.gui_properties["section_1b"]["detection_view"]:
+								cv2.drawContours(visible_img, [approx], -1, TASK_COLOR, LINE_THICKNESS)
+								cv2.circle(visible_img, (centerX, centerY), DOT_SIZE, TASK_COLOR, -1)
+										
+								cv2.putText(visible_img, key, 
+											(centerX - 5, centerY - 15), 
 											FONT_BIG, 
 											TEXT_SIZE_BIG, 
-											SOLUTION_COLOR, 
+											FONT_COLOR_TASK, 
 											BOLD, 
 											cv2.LINE_AA)
-
-									
-							cv2.drawContours(img_rgb, [approx], -1, TASK_COLOR, LINE_THICKNESS)
-							cv2.circle(img_rgb, (centerX, centerY), DOT_SIZE, TASK_COLOR, -1)
-									
-							cv2.putText(img_rgb, key, 
-										(centerX - 5, centerY - 15), 
-										FONT_BIG, 
-										TEXT_SIZE_BIG, 
-										FONT_COLOR_TASK, 
-										BOLD, 
-										cv2.LINE_AA)
 							break
 					if task_found:
 						break
-	return img_rgb
+	return visible_img
 
 
 def get_robot_radius(points):
